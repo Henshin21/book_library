640120acc75c8455870ad3100b29a9cca556882c
Tai Cao <Henshin21@yahoo.pl>
2023-01-23 22:11:17 +0100

    added function add books

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
@@ -15,3 +15,16 @@ class BookForm(FlaskForm):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add-book', methods=['GET', 'POST'])
+def add_book():
+    form = BookForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        author = form.author.data
+        description = form.description.data
+        release_date = form.release_date.data
+        
+        return redirect(url_for('books'))
+    return render_template('add_book.html', form=form)
+
